c76d2577d44cd38c0a6c254cbbdc98ae6c800db5
mooracle <yanuar.prakosa@gmail.com>
2019-03-05 14:22:53 +0700

    Wrapping Your UI Code

--- app/src/main/java/com/ideproject/mooracle/recommendations/MainActivity.java
+++ app/src/main/java/com/ideproject/mooracle/recommendations/MainActivity.java
@@ -2,12 +2,44 @@ package com.ideproject.mooracle.recommendations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //field for views
+    private RecyclerView recyclerView;
+    private View progressBar;
+    private TextView errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //prepares views by ids
+        recyclerView = findViewById(R.id.recycler_view);
+        progressBar = findViewById(R.id.progress_bar);
+        errorText = findViewById(R.id.error_text);
+
+        showLoading();
+    }
+
+    private void showLoading(){
+        recyclerView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        errorText.setVisibility(View.GONE);
+    }
+
+    private void showList(){
+        recyclerView.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
+        errorText.setVisibility(View.GONE);
+    }
+
+    private void showError(){
+        recyclerView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+        errorText.setVisibility(View.VISIBLE);
     }
 }
