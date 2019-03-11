580b6fa8ca3ca89ec7a1a24d65a8aea9801cd53d
mooracle <yanuar.prakosa@gmail.com>
2019-03-11 13:45:09 +0700

    State Handling of GoogleApiClient

--- app/src/main/java/com/ideproject/mooracle/recommendations/google/GoogleServicesHelper.java
+++ app/src/main/java/com/ideproject/mooracle/recommendations/google/GoogleServicesHelper.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
@@ -28,6 +29,10 @@ GoogleApiClient.OnConnectionFailedListener{
     private Activity activity;
     private GoogleServicesListener listener;
 
+    //static constant request codes:
+    private static final int REQUEST_CODE_RESOLUTION = -100;
+    private static final int REQUEST_CODE_AVAILABILITY = -101;
+
     //constructor:
 
 
@@ -43,6 +48,42 @@ GoogleApiClient.OnConnectionFailedListener{
                 .addConnectionCallbacks(this) //since this class is the one which handles this callbacks not activity
                 .addOnConnectionFailedListener(this) //same as above resoning
                 .build();
+    }
+
+    /*  According to the documentation, you should initiate talking to Google Play services by calling connect in on start
+        and disconnect in on stop of your activity.
+     */
+    public void connect(){
+        apiClient.connect();
+    }
+    public void disconnect(){
+        apiClient.disconnect();
+    }
+
+    /*  And one thing to think about when we're writing code like this is that what happens if Google Play services
+        isn't available, what should we do then? Well, we should check that and handle those cases to make sure our code
+        doesn't crash, or throw a null pointer exception, or another error. So let's go ahead and add in a method, and
+        what this is gonna do is just check if Google Play services is possibly available for us to connect to.
+        */
+    private boolean isGooglePlayServicesAvailable(){
+        //check availability by using GoogleApiAvailability since the one in GoogleApiServicesUtil is already deprecated
+        // you cannot instantiate directly GoogleApiAvailability instead you need to getInstace() to get static version
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int availability = apiAvailability.isGooglePlayServicesAvailable(activity);
+
+        //test case using switch for all possible availability results:
+        switch (availability){
+            case ConnectionResult.SUCCESS:
+                return true;
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+            case ConnectionResult.SERVICE_DISABLED:
+            case ConnectionResult.SERVICE_INVALID:
+                //get the error dialog for the user:
+                apiAvailability.getErrorDialog(activity, availability, REQUEST_CODE_AVAILABILITY).show();
+                return false;
+
+                default: return false;
+        }
     }
 
     @Override
